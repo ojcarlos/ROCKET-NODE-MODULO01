77d41500a6d272b0343380b002a41e9dc0b76f3f
ojcarlos <carlos.ojunior00@gmail.com>
2021-11-11 16:25:57 -0400

    uuid configurado

--- src/index.js
+++ src/index.js
@@ -1,26 +1,12 @@
  const express = require("express");
-
+const {v4: uuidv4} = require("uuid")
  const app = express();
 
-app.get("/courses", (req, res) => {
-    return res.json(["curso 1", "curso 2", "curso 3"]);
+
+
+app.post("/account", (req, res) => {
+   const {cpf, name} = request.body;
 })
 
-app.post("/courses", (req, res) => {
-    return res.json(["curso 1", "curso 2", "curso 3", "curso 4"]);
-})
 
-app.put("/courses/:id", (req, res) => {
-    const params = req.params;
-    console.log(params);
-    return res.json(["curso 6", "curso 2", "curso 3", "curso 4"]);
-})
-
-app.patch("/courses/:id", (req, res) => {
-    return res.json(["curso 6", "curso 7", "curso 3", "curso 4"]);
-})
-
-app.delete("/courses/:id", (req, res) =>{
-    return res.json(["curso 6", "curso 7", "curso 4"]);
-})
  app.listen(3333);
