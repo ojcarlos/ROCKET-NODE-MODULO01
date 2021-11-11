280499d30511d0a8754765becb48982906fc4276
ojcarlos <carlos.ojunior00@gmail.com>
2021-11-11 16:42:22 -0400

    inicio do cadastro

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
  const express = require("express");
-const {v4: uuidv4} = require("uuid")
+ const {v4: uuidv4} = require("uuid")
  const app = express();
 
+const customers = []
 
+app.use(express.json())
 
 app.post("/account", (req, res) => {
-   const {cpf, name} = request.body;
+   const {cpf, name} = req.body;
+   const id = uuidv4();
+
+   customers.push({
+       cpf,
+       name,
+       id,
+       statement: []
+   });
+
+   return res.status(201).send();
+
 })
 
 
