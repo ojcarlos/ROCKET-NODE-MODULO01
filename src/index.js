3c65e91ad57e71f3acc736cc78a23c11db9c38a0
ojcarlos <carlos.ojunior00@gmail.com>
2021-11-22 13:30:20 -0400

    finalizado

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
- const express = require("express");
+
+const express = require("express");
  const {v4: uuidv4} = require("uuid")
  const app = express();
 
@@ -6,28 +7,131 @@ const customers = []
 
 app.use(express.json())
 
+function verifyIfExistsAccountCPF(req, res, next) {
+    const {cpf} = req.headers;
+    const customer = customers.find((customer) => customer.cpf === cpf)
+    if(!customer){
+        return res.status(404).json({error: 'Customer not found'})
+    }
+    req.customer = customer
+    return next()
+}
+
+
+//app.use(verifyIfExistsAccountCPF);
+
+
+function getBalance(statement){
+    const balance = statement.reduce((acc,operation)=>{
+        if(operation.type === "credit"){
+            return acc + operation.amount;
+        }
+        else {
+            return acc - operation.amount;
+        }
+    },0)
+    return balance
+}
 app.post("/account", (req, res) => {
-   const {cpf, name} = req.body;
+    const {cpf, name} = req.body;
+ 
+    const customerExists = customers.some(
+        (customer) => customer.cpf === cpf
+        );
+ 
+    if(customerExists){
+     return res.status(400).send({error: "customer already exists"});
+    }
+    
+ 
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+    });
+ 
+    return res.status(201).send();
+ 
+ })
+ 
+app.get("/statement", verifyIfExistsAccountCPF, (req, res)=>{
+    const {customer} = req;
+    
+    return res.json(customer.statement)
+})
 
-   const customerExists = customers.some(
-       (customer) => customer.cpf === cpf
-       );
+app.post("/deposit", verifyIfExistsAccountCPF, (req, res)=>{
+    const {description, amount} = req.body;
+    const { customer } = req;
 
-   if(customerExists){
-    return res.status(400).send({error: "coustumer already exists"});
-   }
-   
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: 'credit'
 
-   customers.push({
-       cpf,
-       name,
-       id: uuidv4(),
-       statement: []
-   });
+    }
+    customer.statement.push(statementOperation);
 
-   return res.status(201).send();
+    return res.status(201).send();
+})
 
+app.post("/withdraw", verifyIfExistsAccountCPF, (req, res)=>{
+    const {description, amount} = req.body;
+    const { customer } = req;
+
+    const balance = getBalance(customer.statement);
+
+    if(balance < amount ){
+        return res.status(400).json({error: " sem grana, irmão"})
+    }
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: 'debit'
+
+    }
+    customer.statement.push(statementOperation);
+
+    return res.status(201).send();
 })
 
 
+app.get("/statement/date", verifyIfExistsAccountCPF, (req, res)=>{
+    const {customer} = req;
+    const {date} = req.query;
+
+    const dateFormat = new Date(date + " 00:00")
+
+    const statement = customer.statement.filter((statement) => 
+    statement.created_at.toDateString() === new Date(dateFormat).toDateString())
+    
+    return res.json(statement)
+})
+
+app.put("/account",verifyIfExistsAccountCPF, (req, res)=>{
+    const {name} = req.body;
+    const {customer} = req;
+
+    customer.name = name;
+    
+    return res.status(200).send();
+})
+app.get("/account",verifyIfExistsAccountCPF, (req, res)=>{
+    const {customer} = req;
+
+    
+    return res.json(customers)
+})
+
+app.delete("/account",verifyIfExistsAccountCPF, (req, res)=>{
+    const {customer} = req;
+
+    customers.splice(customer, 1);
+    
+    return res.status(204).json(customers)
+})
  app.listen(3333);
